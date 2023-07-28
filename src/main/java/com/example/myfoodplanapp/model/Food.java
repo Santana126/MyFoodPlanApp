d6bc2f6e9055af4c317ee38456ca44381591a6a0
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-28 08:25:08 +0200

    Update Meal and Food
    Meal and Food classes updated.

--- src/main/java/com/example/myfoodplanapp/model/Food.java
+++ src/main/java/com/example/myfoodplanapp/model/Food.java
@@ -8,4 +8,37 @@ public class Food {
     private int fats;
 
     // Costruttore e metodi getter/setter
+
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(int proteins) {
+        this.proteins = proteins;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getFats() {
+        return fats;
+    }
+
+    public void setFats(int fats) {
+        this.fats = fats;
+    }
 }
--- src/main/java/com/example/myfoodplanapp/model/Meal.java
+++ src/main/java/com/example/myfoodplanapp/model/Meal.java
@@ -1,14 +1,72 @@
 package com.example.myfoodplanapp.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Meal {
-    private List<Food> foods;
-    private String mealType;
-    private int totalCalories;
-    private int totalProteins;
-    private int totalCarbs;
-    private int totalFats;
+    private List<Food> foods; // Elenco dei cibi consumati durante il pasto
+    private String mealType; // Tipo di pasto (colazione, pranzo, cena, ecc.)
+    private int totalCalories; // Calorie totali del pasto
+    private int totalProteins; // Proteine totali del pasto
+    private int totalCarbs; // Carboidrati totali del pasto
+    private int totalFats; // Grassi totali del pasto
 
-    // Costruttore e metodi getter/setter
+    // Costruttore della classe Meal
+    public Meal() {
+        this.foods = new ArrayList<>();
+        this.mealType = "";
+        this.totalCalories = 0;
+        this.totalProteins = 0;
+        this.totalCarbs = 0;
+        this.totalFats = 0;
+    }
+
+    // Metodi getter e setter per i campi della classe Meal
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<Food> foods) {
+        this.foods = foods;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    public int getTotalCalories() {
+        return totalCalories;
+    }
+
+    public void setTotalCalories(int totalCalories) {
+        this.totalCalories = totalCalories;
+    }
+
+    public int getTotalProteins() {
+        return totalProteins;
+    }
+
+    public void setTotalProteins(int totalProteins) {
+        this.totalProteins = totalProteins;
+    }
+
+    public int getTotalCarbs() {
+        return totalCarbs;
+    }
+
+    public void setTotalCarbs(int totalCarbs) {
+        this.totalCarbs = totalCarbs;
+    }
+
+    public int getTotalFats() {
+        return totalFats;
+    }
+
+    public void setTotalFats(int totalFats) {
+        this.totalFats = totalFats;
+    }
 }
