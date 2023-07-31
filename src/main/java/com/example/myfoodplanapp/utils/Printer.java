b4c1df3b805728043ea7632e691a5542cf9ac0c5
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-31 18:43:00 +0200

    Create Printer.java

---
+++ src/main/java/com/example/myfoodplanapp/utils/Printer.java
@@ -0,0 +1,14 @@
+package com.example.myfoodplanapp.utils;
+
+public class Printer {
+    //Stampa messaggi sulla CLI
+
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void printSeparator() {
+        System.out.println("\n--------------------------------------------------------------------------------------------\n");
+    }
+}
