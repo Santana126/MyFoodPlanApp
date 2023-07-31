3888e3ef3a111d72c27910cc07aa83240a49e75b
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-31 18:44:37 +0200

    Updated Main

--- src/main/java/com/example/myfoodplanapp/Main.java
+++ src/main/java/com/example/myfoodplanapp/Main.java
@@ -1,5 +1,6 @@
 package com.example.myfoodplanapp;
 
+import com.example.myfoodplanapp.controller.graphicontroller.cli.CLIWelcomePageController;
 import com.example.myfoodplanapp.view.UserInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +9,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -19,5 +20,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        CLIWelcomePageController cliWelcomePageController = new CLIWelcomePageController();
+        cliWelcomePageController.start();
+
     }
 }
