eb1f80e8d50d06484890a6a845a68a8df7dc60af
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-08-01 17:04:00 +0200

    Update CLIWelcomePage
    Add option and done first test

--- src/main/java/com/example/myfoodplanapp/controller/graphicontroller/cli/CLIWelcomePageController.java
+++ src/main/java/com/example/myfoodplanapp/controller/graphicontroller/cli/CLIWelcomePageController.java
@@ -1,5 +1,7 @@
 package com.example.myfoodplanapp.controller.graphicontroller.cli;
 
+import com.example.myfoodplanapp.exception.CommandNotFoundException;
+import com.example.myfoodplanapp.utils.Printer;
 import com.example.myfoodplanapp.view.cli.CLIWelcomePageView;
 
 public class CLIWelcomePageController {
@@ -12,5 +14,20 @@ public class CLIWelcomePageController {
         this.cliWelcomePageView.run();
     }
 
+
+    public void runCommand(String choice) throws CommandNotFoundException {
+        switch (choice) {
+            case "1" -> {
+                Printer.printMessage("Login Pressed");
+            }
+            case "2" -> {
+                Printer.printMessage("Signup Pressed");
+            }
+            case "3" -> {
+                Printer.printMessage("Guest Pressed");
+            }
+            default -> throw new CommandNotFoundException("1) 2) 3)");
+        }
+    }
 
 }
--- src/main/java/com/example/myfoodplanapp/view/cli/CLIWelcomePageView.java
+++ src/main/java/com/example/myfoodplanapp/view/cli/CLIWelcomePageView.java
@@ -1,7 +1,11 @@
 package com.example.myfoodplanapp.view.cli;
 
 import com.example.myfoodplanapp.controller.graphicontroller.cli.CLIWelcomePageController;
+import com.example.myfoodplanapp.exception.CommandNotFoundException;
 import com.example.myfoodplanapp.utils.Printer;
+import com.example.myfoodplanapp.utils.ShowException;
+
+import java.util.Scanner;
 
 public class CLIWelcomePageView {
 
@@ -15,6 +19,22 @@ public class CLIWelcomePageView {
         Printer.printSeparator();
         Printer.printMessage("------------------------------------------WELCOME-------------------------------------------");
         Printer.printSeparator();
+        Printer.printMessage("Choose an option:\n");
+        Printer.printMessage("\n1) Log in   \n2) Sign up   \n3) Guest\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        try{
+            this.cliWelcomePageControllerCurrent.runCommand(input);
+        }catch (CommandNotFoundException e){
+            ShowException.showExceptionCLI(e.getMessage());
+            run();
+        }
+
+
+
+
 
     }
 
